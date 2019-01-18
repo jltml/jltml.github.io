6bd4ec64ed4955a199375b5f61487e9f18127aca
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2019-01-18 00:28:43 -0600

    fixed links on blog and thoughts index pages

--- config.rb
+++ config.rb
@@ -12,6 +12,7 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/README.md', layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
