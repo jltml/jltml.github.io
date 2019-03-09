64b40ef0a4482d0231e9e382dce9f50cd41fcf26
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2019-03-08 23:23:23 -0600

    Automated commit at 2019-03-09 05:23:22 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/photos.js
+++ javascripts/photos.js
@@ -1,4 +1,4 @@
 $(function() {
- var images = ['water1.jpg', 'water2.jpg', 'bridge.jpg', 'siestasunset.jpg', 'palmtrees.jpg', 'fog.jpg', 'reflection.jpg', 'smoke.jpg'];
+ var images = ['water1.jpg', 'water2.jpg', 'bridge.jpg', 'siestasunset.jpg', 'palmtrees.jpg', 'fog.jpg', 'reflection.jpg', 'prarie.jpg'];
  $('body').css({'background-image': 'url(./../images/backgrounds/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
