7b7330df0524bbfa37565fd2b9391982b6c77c41
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2020-11-07 16:32:06 -0600

    more blog improvements and testing lazyloading

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,8 +1,10 @@
-window.addEventListener("load", function(){
+/* window.addEventListener("load", function(){
     $('body').addClass('loaded');
-});
+}); */
 
-window.onload = $(function() {
+window.addEventListener('DOMContentLoaded', (event) => {
  var images = ['water1.jpg', 'water2.jpg', 'bridge.jpg', 'siestasunset1.jpg', 'palmtrees.jpg', 'fog.jpg', 'reflection1.jpg', 'prarie.jpg', 'avalanchelake.jpg', 'bean.jpg', 'bearheadlakeroad.jpg', 'bench.jpg', 'blueorangecarsunset.jpg', 'brycetree.jpg', 'building.jpg', 'canadawater.jpg', 'carwindowrain.jpg', 'coloradosunsetwater.jpg', 'cta.jpg', 'delicatearch.jpg', 'dock.jpg', 'duluthice.jpg', 'dunordtrees.jpg', 'elyevergreens.jpg', 'empiresplash.jpg', 'flowers.jpg', 'frostywindow.jpg', 'goldengate.jpg', 'gradient.jpg', 'grandave.jpg', 'grandportage.jpg', 'lighthouse.jpg', 'lincolnzoo.jpg', 'michiganwave.jpg', 'nightminneapolis.jpg', 'oaklandaerial.jpg', 'oldwomancove.jpg', 'owatonna.jpg', 'pigeonfalls1.jpg', 'pigeonfalls2.jpg', 'railroadbridge.jpg', 'reflection2.jpg', 'rocks.jpg', 'rockymountainrainbow.jpg', 'roosevelt.jpg', 'rooseveltprarie.jpg', 'sealion.jpg', 'sfstreet.jpg', 'siestasunset2.jpg', 'stillwater.jpg', 'sunsetdots.jpg', 'unbuilding.jpg', 'utahstars.jpg', 'waterrocks.jpg', 'watertree.jpg', 'woodwater.jpg', 'weirddashboard.jpg', 'yosemiteview.jpg'];
  $('body').css({'background-image': 'url(./../images/backgrounds/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
+
+// Lazy-loading image stuff below
