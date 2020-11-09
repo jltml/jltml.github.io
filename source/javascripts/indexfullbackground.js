9723008507b8e9175f2344b78d4300b15ff26f81
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2020-11-08 18:41:00 -0600

    made small images smaller... uh oh did lazy-loading actually just work??

--- source/javascripts/indexfullbackground.js
+++
@@ -1,8 +0,0 @@
-function fullBackground() {
-  $('body').css({'background-image': 'url(./../images/backgrounds/full/' + randomImage + ')'});
-  $('.blur').css({'backdrop-filter': 'blur(0px)'});
-}
-
-window.onload {
-  fullBackground();
-});
