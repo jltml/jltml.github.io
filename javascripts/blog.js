9dd3f735f45d13e4211c43b51cf97302854d89ec
jltml <8261330+jltml@users.noreply.github.com>
2021-01-13 00:10:50 -0600

    Automated commit at 2021-01-13 06:10:50 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/blog.js
+++ javascripts/blog.js
@@ -1,6 +1,1 @@
-$('.js-back').on('click', function(evt) {
-  if (document.referrer.indexOf(window.location.host) !== -1){
-    evt.preventDefault();
-    history.back();
-  }
-});
+
--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,18 @@
 // This is where it all goes :)
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if ($("#sidenav").hasClass("show")) {
+    void(0);
+  }
+  else {
+      if ((prevScrollpos > currentScrollPos) || (currentScrollPos <= 0 ) || ((window.innerHeight + window.scrollY) >= document.body.offsetHeight))  {
+        $("#banner").fadeIn();
+      } else {
+        $("#banner").fadeOut();
+      }
+    prevScrollpos = currentScrollPos;
+    }
+  }
