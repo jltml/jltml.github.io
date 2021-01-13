a70d016b26dea0f3f1cac09cc95fa0424ef46e2a
jltml <8261330+jltml@users.noreply.github.com>
2021-01-13 09:55:17 -0600

    Automated commit at 2021-01-13 15:55:17 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,6 +1,32 @@
 // This is where it all goes :)
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// Scrolling inertia, from StackOverflow
+var checkScrollSpeed = (function(settings){
+    settings = settings || {};
+
+    var lastPos, newPos, timer, delta,
+        delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+    function clear() {
+      lastPos = null;
+      delta = 0;
+    }
+
+    clear();
+
+    return function(){
+      newPos = window.scrollY;
+      if ( lastPos != null ){ // && newPos < maxScroll
+        delta = newPos -  lastPos;
+      }
+      lastPos = newPos;
+      clearTimeout(timer);
+      timer = setTimeout(clear, delay);
+      return delta;
+    };
+})();
+
+// Show/hide banne based on scroll direction and inertia (built off W3Schools example)
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -8,9 +34,10 @@ window.onscroll = function() {
     void(0);
   }
   else {
-      if ((prevScrollpos > currentScrollPos) || (currentScrollPos <= 0 ) || ((window.innerHeight + window.scrollY) >= document.body.offsetHeight))  {
+      if (((prevScrollpos > currentScrollPos) && (Math.abs(checkScrollSpeed()) > 5)) || (currentScrollPos <= 0 ) || ((window.innerHeight + window.scrollY) >= document.body.offsetHeight))  {
         $("#banner").fadeIn();
-      } else {
+      }
+      else if (prevScrollpos < currentScrollPos) {
         $("#banner").fadeOut();
       }
     prevScrollpos = currentScrollPos;
