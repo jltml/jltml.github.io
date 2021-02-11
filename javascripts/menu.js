295b7b52b33db3ba38fe0e0cdc92274eeeedd34e
jltml <8261330+jltml@users.noreply.github.com>
2021-02-10 22:48:53 -0600

    Automated commit at 2021-02-11 04:48:53 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,1 +1,60 @@
-function showMenu(){sidenav.classList.add("show"),sidenavEdge.classList.add("show"),menuButton.classList.add("hide"),$(".blog-article-link-etc *").css({"z-index":"-1"})}function hideMenu(){sidenav.classList.remove("show"),sidenavEdge.classList.remove("show"),menuButton.classList.remove("hide"),$(".blog-article-link-etc *").css({"z-index":"1"})}function openEdge(){document.getElementById("banner").style.background="#FFFFFF"}function toggleProfiles(){$(".menu-profiles").slideToggle("slow"),$(".default-menu-items").slideToggle("slow")}function toggleProfilesInstagram(){$(".menu-profiles").slideToggle("slow"),$(".menu-profiles-instagram").slideToggle("slow")}var menuButton=document.querySelector("#menuButton");onload=function(){document.getElementById("lastModified").innerHTML="last updated "+document.lastModified,document.getElementById("lastModified").setAttribute("style","font-size: 3vmin")};
+/*
+var sidenav = document.querySelector("div#sidenav:not(.exclude)");
+sidenav.addEventListener("click", hideMenu, false);
+*/
+
+var menuButton = document.querySelector("#menuButton");
+
+function showMenu(e) {
+    sidenav.classList.add("show");
+    sidenavEdge.classList.add("show");
+    menuButton.classList.add("hide");
+    // Workaround because I can't get z-index to work :( please fix this someday
+    $('.blog-article-link-etc *').css({'z-index': '-1'});
+}
+
+function hideMenu(e) {
+    sidenav.classList.remove("show");
+    sidenavEdge.classList.remove("show");
+    menuButton.classList.remove("hide");
+    // Workaround because I can't get z-index to work :( please fix this someday
+    $('.blog-article-link-etc *').css({'z-index': '1'});
+}
+
+function openEdge() {
+  document.getElementById("banner").style.background = "#FFFFFF";
+}
+
+onload = function() {
+  document.getElementById("lastModified").innerHTML = "last updated " + document.lastModified;
+  document.getElementById("lastModified").setAttribute(
+      "style", "font-size: 3vmin");
+}
+
+function toggleProfiles() {
+  $(".menu-profiles").slideToggle("slow");
+  $(".default-menu-items").slideToggle("slow");
+}
+
+function toggleProfilesInstagram() {
+  $(".menu-profiles").slideToggle("slow");
+  $(".menu-profiles-instagram").slideToggle("slow");
+}
+
+/*
+$(function stopScoll() {
+
+    var fixed = document.getElementById('fixed'), overflow;
+
+    $(window).on('load resize', function() {
+
+    overflow = fixed.scrollHeight-$('#fixed').height();
+    });
+
+    fixed.on('touchmove', function() {
+
+    if (overflow) return true;
+    else return false;
+    });
+});
+*/
--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,45 @@
-var checkScrollSpeed=function(e){function o(){n=null,c=0}e=e||{};var n,l,r,c,s=e.delay||50;return o(),function(){return l=window.scrollY,null!=n&&(c=l-n),n=l,clearTimeout(r),r=setTimeout(o,s),c}}(),prevScrollpos=window.pageYOffset;window.onscroll=function(){var e=window.pageYOffset;$("#sidenav").hasClass("show")||(prevScrollpos>e&&Math.abs(checkScrollSpeed())>5||e<=0||window.innerHeight+window.scrollY>=document.body.offsetHeight?$("#banner").fadeIn():prevScrollpos<e&&$("#banner").fadeOut(),prevScrollpos=e)};
+// This is where it all goes :)
+
+// Scrolling inertia, from StackOverflow
+var checkScrollSpeed = (function(settings){
+    settings = settings || {};
+
+    var lastPos, newPos, timer, delta,
+        delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+    function clear() {
+      lastPos = null;
+      delta = 0;
+    }
+
+    clear();
+
+    return function(){
+      newPos = window.scrollY;
+      if ( lastPos != null ){ // && newPos < maxScroll
+        delta = newPos -  lastPos;
+      }
+      lastPos = newPos;
+      clearTimeout(timer);
+      timer = setTimeout(clear, delay);
+      return delta;
+    };
+})();
+
+// Show/hide banne based on scroll direction and inertia (built off W3Schools example)
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if ($("#sidenav").hasClass("show")) {
+    void(0);
+  }
+  else {
+      if (((prevScrollpos > currentScrollPos) && (Math.abs(checkScrollSpeed()) > 5)) || (currentScrollPos <= 0 ) || ((window.innerHeight + window.scrollY) >= document.body.offsetHeight))  {
+        $("#banner").fadeIn();
+      }
+      else if (prevScrollpos < currentScrollPos) {
+        $("#banner").fadeOut();
+      }
+    prevScrollpos = currentScrollPos;
+    }
+  }
